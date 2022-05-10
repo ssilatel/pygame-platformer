b424b4e4d65ad7e268e08af7a700b074dd7e5f49
ssilatel <72793669+ssilatel@users.noreply.github.com>
2022-05-10 13:52:41 +0200

    Add files via upload

--- a.py
+++ a.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import engine as e
 
 clock = pygame.time.Clock()
 SCREEN_WIDTH = 1280
@@ -8,95 +9,11 @@ TILE_SIZE = 64
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 scroll = [0, 0]
 
-def load_map(path):
-    f = open(path, "r")
-    data = f.read()
-    f.close()
-    data = data.split("\n")
-    game_map = []
-    for line in data:
-        game_map.append(list(line))
-    return game_map
 
-game_map = load_map("test_map.txt")
+game_map = e.load_map("test_map.txt")
 dirt_img = pygame.Surface((TILE_SIZE, TILE_SIZE))
 dirt_img.fill((100, 100, 100))
-
-def collision_test(rect, tiles):
-    hit_list = []
-    for tile in tiles:
-        if rect.colliderect(tile):
-            hit_list.append(tile)
-    return hit_list
-
-class Player():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.w = TILE_SIZE // 2
-        self.h = TILE_SIZE // 2
-        self.image = pygame.Surface((self.w, self.h))
-        self.image.fill((175, 100, 175))
-        self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
-        self.movement = [0, 0]
-        self.moving_left = False
-        self.moving_right = False
-        self.y_momentum = 0
-        self.air_timer = 0
-    
-    def move(self, tiles):
-        collision_types = {"top": False, "bottom": False, "left": False, "right": False}
-        self.rect.x += self.movement[0]
-        hit_list = collision_test(self.rect, tiles)
-        for tile in hit_list:
-            if self.movement[0] < 0:
-                self.rect.left = tile.right
-                collision_types["left"] = True
-            elif self.movement[0] > 0:
-                self.rect.right = tile.left
-                collision_types["right"] = True
-        self.rect.y += self.movement[1]
-        hit_list = collision_test(self.rect, tiles)
-        for tile in hit_list:
-            if self.movement[1] < 0:
-                self.rect.top = tile.bottom
-                collision_types["top"] = True
-            elif self.movement[1] > 0:
-                self.rect.bottom = tile.top
-                collision_types["bottom"] = True
-        return collision_types
-    
-    def update(self, tiles):
-        self.movement = [0, 0]
-        if self.moving_left:
-            self.movement[0] -= 5
-        elif self.moving_right:
-            self.movement[0] += 5
-        self.movement[1] += self.y_momentum
-        self.y_momentum += 1.5
-        if self.y_momentum > 8:
-            self.y_momentum = 8
-        
-        collisions = self.move(tiles)
-        if collisions["bottom"]:
-            self.y_momentum = 1
-            self.air_timer = 0
-        else:
-            self.air_timer += 1
-        if collisions["top"]:
-            self.y_momentum = 0
-    
-    def draw(self, screen, scroll):
-        screen.blit(self.image, (self.rect.x - scroll[0], self.rect.y - scroll[1]))
-    
-    def set_pos(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def get_pos(self):
-        return [self.x, self.y]
-
-player = Player(100, 800)
+player = e.Player(300, 800, TILE_SIZE // 2, TILE_SIZE // 2)
 
 while True:
     for event in pygame.event.get():
---
+++ engine.py
@@ -0,0 +1,80 @@
+import pygame
+
+def load_map(path):
+    f = open(path, "r")
+    data = f.read()
+    f.close()
+    data = data.split("\n")
+    game_map = []
+    for line in data:
+        game_map.append(list(line))
+    return game_map
+
+def collision_test(rect, tiles):
+    hit_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
+
+def move(rect, movement, tiles):
+    collision_types = {"top": False, "bottom": False, "left": False, "right": False}
+    rect.x += movement[0]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[0] < 0:
+            rect.left = tile.right
+            collision_types["left"] = True
+        elif movement[0] > 0:
+            rect.right = tile.left
+            collision_types["right"] = True
+    rect.y += movement[1]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[1] < 0:
+            rect.top = tile.bottom
+            collision_types["top"] = True
+        elif movement[1] > 0:
+            rect.bottom = tile.top
+            collision_types["bottom"] = True
+    return collision_types
+
+class Player():
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.image = pygame.Surface((self.w, self.h))
+        self.image.fill((175, 100, 175))
+        self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
+        self.movement = [0, 0]
+        self.speed = 5
+        self.moving_left = False
+        self.moving_right = False
+        self.y_momentum = 0
+        self.air_timer = 0
+    
+    
+    def update(self, tiles):
+        self.movement = [0, 0]
+        if self.moving_left:
+            self.movement[0] -= self.speed
+        elif self.moving_right:
+            self.movement[0] += self.speed
+        self.movement[1] += self.y_momentum
+        self.y_momentum += 1.5
+        if self.y_momentum > 8:
+            self.y_momentum = 8
+        
+        collisions = move(self.rect, self.movement, tiles)
+        if collisions["bottom"]:
+            self.y_momentum = 1
+            self.air_timer = 0
+        else:
+            self.air_timer += 1
+        if collisions["top"]:
+            self.y_momentum = 0
+        
+    def draw(self, screen, scroll):
+        screen.blit(self.image, (self.rect.x - scroll[0], self.rect.y - scroll[1]))
