b2391e64341a1d2fd1ec76a97941e991660b2132
ssilatel <72793669+ssilatel@users.noreply.github.com>
2022-05-11 15:54:06 +0200

    Add files via upload

--- engine.py
+++ engine.py
@@ -17,26 +17,36 @@ def collision_test(rect, tiles):
             hit_list.append(tile)
     return hit_list
 
-def move(rect, movement, tiles):
-    collision_types = {"top": False, "bottom": False, "left": False, "right": False}
+def move(rect, movement, tiles, mode, screen, scroll):
+    collision_types = {"top": False, "bottom": False, "left": False, "right": False, "wall_climb": False}
     rect.x += movement[0]
     hit_list = collision_test(rect, tiles)
     for tile in hit_list:
         if movement[0] < 0:
             rect.left = tile.right
             collision_types["left"] = True
+            if mode == "debug":
+                pygame.draw.rect(screen, (255, 0, 0), (tile.x - scroll[0], tile.y - scroll[1], tile.w, tile.h))
         elif movement[0] > 0:
             rect.right = tile.left
             collision_types["right"] = True
+            if mode == "debug":
+                pygame.draw.rect(screen, (255, 0, 0), (tile.x - scroll[0], tile.y - scroll[1], tile.w, tile.h))
+        if (collision_types["left"] or collision_types["right"]) and rect.top > tile.top:
+            collision_types["wall_climb"] = True
     rect.y += movement[1]
     hit_list = collision_test(rect, tiles)
     for tile in hit_list:
         if movement[1] < 0:
             rect.top = tile.bottom
             collision_types["top"] = True
+            if mode == "debug":
+                pygame.draw.rect(screen, (255, 0, 0), (tile.x - scroll[0], tile.y - scroll[1], tile.w, tile.h))
         elif movement[1] > 0:
             rect.bottom = tile.top
             collision_types["bottom"] = True
+            if mode == "debug":
+                pygame.draw.rect(screen, (255, 0, 0), (tile.x - scroll[0], tile.y - scroll[1], tile.w, tile.h))
     return collision_types
 
 class Player():
@@ -52,11 +62,15 @@ class Player():
         self.speed = 5
         self.moving_left = False
         self.moving_right = False
+        self.jumping = False
+        self.on_wall = False
         self.y_momentum = 0
         self.air_timer = 0
+        self.jumping_timer = 0
+        self.facing_right = True
     
     
-    def update(self, tiles):
+    def update(self, tiles, mode, screen, scroll):
         self.movement = [0, 0]
         if self.moving_left:
             self.movement[0] -= self.speed
@@ -67,14 +81,27 @@ class Player():
         if self.y_momentum > 8:
             self.y_momentum = 8
         
-        collisions = move(self.rect, self.movement, tiles)
+        collisions = move(self.rect, self.movement, tiles, mode, screen, scroll)
         if collisions["bottom"]:
+            self.can_jump = True
+            self.jumping = False
             self.y_momentum = 1
             self.air_timer = 0
         else:
             self.air_timer += 1
         if collisions["top"]:
             self.y_momentum = 0
-        
+        if self.jumping:
+            self.jumping_timer += 1
+        else:
+            self.jumping_timer = 0
+        if collisions["wall_climb"] and self.jumping_timer > 20:
+            self.on_wall = True
+        else:
+            self.on_wall = False
+        if self.on_wall:
+            self.y_momentum = 0
+        print(collisions["wall_climb"])
+
     def draw(self, screen, scroll):
         screen.blit(self.image, (self.rect.x - scroll[0], self.rect.y - scroll[1]))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import engine as e
+import math
 
 clock = pygame.time.Clock()
 pygame.font.init()
@@ -10,13 +11,13 @@ TILE_SIZE = 64
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 scroll = [0, 0]
 
-
 game_map = e.load_map("test_map.txt")
 dirt_img = pygame.Surface((TILE_SIZE, TILE_SIZE))
 dirt_img.fill((100, 100, 100))
-player = e.Player(100, 700, TILE_SIZE // 2, TILE_SIZE // 2)
+player = e.Player(100, 900, TILE_SIZE // 2, TILE_SIZE // 2)
 
 def game_loop():
+    mode = "prod"
     running = True
     while running:
         for event in pygame.event.get():
@@ -26,17 +27,38 @@ def game_loop():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+                    player.moving_left = False
+                    player.moving_right = False
+                    pause_menu()
                 if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    player.moving_right = False
                     player.moving_left = True
+                    player.facing_right = False
                 if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    player.moving_left = False
                     player.moving_right = True
-                if event.key == pygame.K_UP or event.key == pygame.K_w or event.key == pygame.K_SPACE:
-                    if player.air_timer < 6:
-                        player.y_momentum = -20
+                    player.facing_right = True
+                if (event.key == pygame.K_UP or event.key == pygame.K_w or event.key == pygame.K_SPACE):
+                    if player.on_wall and player.jumping:
+                        player.on_wall = False
+                        player.y_momentum -= 20
+                        player.jumping_timer = 0
+                        if player.facing_right:
+                            pass
+                        else:
+                            pass
+                    if player.air_timer < 6 and not player.jumping:
+                        player.y_momentum -= 20
+                        player.jumping = True
                 if event.key == pygame.K_r:
                     player.rect.x = 100
-                    player.rect.y = 700
+                    player.rect.y = 900
                     player.movement = [0, 0]
+                if event.key == pygame.K_TAB:
+                    if mode == "prod":
+                        mode = "debug"
+                    elif mode == "debug":
+                        mode = "prod"
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_a:
                     player.moving_left = False
@@ -60,7 +82,7 @@ def game_loop():
                 x += 1
             y += 1
         
-        player.update(tile_rects)
+        player.update(tile_rects, mode, screen, scroll)
         player.draw(screen, scroll)
         
         pygame.display.update()
@@ -74,41 +96,85 @@ def draw_menu_font(x, y, size, text, color, selected):
     if selected:
         pygame.draw.rect(screen, (255, 255, 255), ((x - size[0] // 2) - 50 // 2, y - 5, 8, 8))
 
-options = [["PLAY", 50], ["QUIT", 40]]
-selected_choice = 0
-choices = []
-for o in options:
-    choices.append([o[0], False])
-print(choices)
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if choices[selected_choice][0] == "PLAY":
+def main_menu():
+    options = [["PLAY", 50], ["OPTIONS", 40], ["QUIT", 40]]
+    selected_choice = 0
+    choices = []
+    for o in options:
+        choices.append([o[0], False])
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if choices[selected_choice][0] == "PLAY":
+                        game_loop()
+                    elif choices[selected_choice][0] == "QUIT":
+                        pygame.quit()
+                        sys.exit()
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    selected_choice -= 1
+                    if (selected_choice) < 0:
+                        selected_choice = len(choices) - 1
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    selected_choice += 1
+                    if selected_choice > len(choices) - 1:
+                        selected_choice = 0
+        
+        for c in choices:
+            c[1] = False
+        choices[selected_choice][1] = True
+        
+        screen.fill((80, 30, 80))
+
+        for i, option in enumerate(options):
+            draw_menu_font(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + (50 * i), option[1], option[0], (255, 255, 255), choices[i][1])
+
+        pygame.display.update()
+        clock.tick(60)
+
+def pause_menu():
+    options = [["RESUME", 50], ["OPTIONS", 40], ["QUIT", 40]]
+    selected_choice = 0
+    choices = []
+    for o in options:
+        choices.append([o[0], False])
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
                     game_loop()
-                elif choices[selected_choice][0] == "QUIT":
-                    pygame.quit()
-                    sys.exit()
-            if event.key == pygame.K_UP:
-                selected_choice -= 1
-                if (selected_choice) < 0:
-                    selected_choice = len(choices) - 1
-            if event.key == pygame.K_DOWN:
-                selected_choice += 1
-                if selected_choice > len(choices) - 1:
-                    selected_choice = 0
-    
-    for c in choices:
-        c[1] = False
-    choices[selected_choice][1] = True
-    
-    screen.fill((80, 30, 80))
+                if event.key == pygame.K_SPACE:
+                    if choices[selected_choice][0] == "RESUME":
+                        game_loop()
+                    elif choices[selected_choice][0] == "QUIT":
+                        pygame.quit()
+                        sys.exit()
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    selected_choice -= 1
+                    if (selected_choice) < 0:
+                        selected_choice = len(choices) - 1
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    selected_choice += 1
+                    if selected_choice > len(choices) - 1:
+                        selected_choice = 0
+        
+        for c in choices:
+            c[1] = False
+        choices[selected_choice][1] = True
+        
+        screen.fill((60, 3, 95))
 
-    for i, option in enumerate(options):
-        draw_menu_font(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + (50 * i), option[1], option[0], (255, 255, 255), choices[i][1])
+        for i, option in enumerate(options):
+            draw_menu_font(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + (50 * i), option[1], option[0], (255, 255, 255), choices[i][1])
 
-    pygame.display.update()
-    clock.tick(60)
+        pygame.display.update()
+        clock.tick(60)
+
+if __name__ == "__main__":
+    main_menu()
